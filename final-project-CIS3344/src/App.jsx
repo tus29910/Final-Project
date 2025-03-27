da18fe70e05afea789362ffddf637e8603a2aa8d
Tonesman44 <antonioman44@gmail.com>
2025-03-27 14:01:33 -0400

    Example of fetching API data

--- final-project-CIS3344/src/App.jsx
+++ final-project-CIS3344/src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const fetchMovies = () => {
+    fetch(`https://api.themoviedb.org/3/search/movie?query=Jack+Reacher&api_key=7d11d871a2508f89d683e26fcc9d0336`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Movies</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={fetchMovies}>
+          Fetch me a movie
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
