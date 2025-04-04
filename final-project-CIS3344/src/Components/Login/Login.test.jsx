ca2e67eea6b17cee3781598937e06c4cb3d98700
Billy Nguyen <tus29910@temple.edu>
2025-04-04 13:56:53 -0500

    Test Passing
    Passing test files

--- final-project-CIS3344/src/Components/Login/Login.test.jsx
+++ final-project-CIS3344/src/Components/Login/Login.test.jsx
@@ -1,13 +1,11 @@
-
 import { render, screen } from "@testing-library/react";
-import Login from "Components/Login/Login";
-
-
+import Login from "./Login";
+import {describe, it, expect} from "vitest";
 
 describe('Login Component', () => {
     it("Renders a default login", () => {
         render(<Login />);
-        const loginElement = screen.getByText("Verify Username");
-        expect(loginElement).toBeInTheDocument();
+        const loginElement = screen.getByText("Verify Login");
+        expect(loginElement).to.exist;
     })
 });
--- final-project-CIS3344/vite.config.js
+++ final-project-CIS3344/vite.config.js
@@ -9,5 +9,5 @@ export default defineConfig({
       provider: "v8",
     },
   },
-  // plugins: [react()],
+  plugins: [react()],
 });
