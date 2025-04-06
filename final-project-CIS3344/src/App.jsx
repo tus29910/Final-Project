6ef139be36779081adaed60cb75c3e24c7b26b4b
Tonesman44 <antonioman44@gmail.com>
2025-04-06 17:11:51 -0400

    User data, profile using user data

--- final-project-CIS3344/src/App.jsx
+++ final-project-CIS3344/src/App.jsx
@@ -5,13 +5,18 @@ import Profile from './Components/Profile/Profile';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
 import MovieDetailView from './Components/DetailView/MovieDetailView';
+import users from './data/users.json';
+import movies from './data/movies.json';
 
 function App() {
+  const currentUser = users[0];
+  // Replace with login state later
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<Profile user={currentUser} movies={movies} />} />
         <Route path="/login" element={<Login />} />
         <Route path="/movieDetailView" element={<MovieDetailView />} />        
       </Routes>
